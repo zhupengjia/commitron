5f3af6e8f9f452ae35683b526466dcbd2f24dbc0
John Stilia <stilia.johny@gmail.com>
2025-03-31 22:19:33 +0100

    fix(commitron): update sha256 checksum for release
    Update the sha256 checksum for version 0.1.0 in the Commitron formula to ensure the integrity of the downloaded package. This change reflects the correct hash value after the release of the tarball.

--- Formula/commitron.rb
+++ Formula/commitron.rb
@@ -3,7 +3,7 @@ class Commitron < Formula
   homepage "https://github.com/stiliajohny/commitron"
   url "https://github.com/stiliajohny/commitron/archive/refs/tags/v0.1.0.tar.gz"
   version "0.1.0"
-  sha256 "45d8c8d78fce2f45e0bd048c641b74af2230d21293a96d7344673de3e7491d74"
+  sha256 "e4bdbd6b4e285a4739c807d5add6033972ee13e155a93b17baa8333da96ada24"
 
   depends_on "go" => :build
 
