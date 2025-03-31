ff311a60b3130d3d18de35c8b60955c6d6383854
John Stilia <stilia.johny@gmail.com>
2025-03-31 22:23:15 +0100

    chore: prepare Formula for v0.1.0 release

--- Formula/commitron.rb
+++ Formula/commitron.rb
@@ -3,7 +3,8 @@ class Commitron < Formula
   homepage "https://github.com/stiliajohny/commitron"
   url "https://github.com/stiliajohny/commitron/archive/refs/tags/v0.1.0.tar.gz"
   version "0.1.0"
-  sha256 "e4bdbd6b4e285a4739c807d5add6033972ee13e155a93b17baa8333da96ada24"
+  # Replace this line with the SHA256 you got from step 6
+  sha256 "PASTE_THE_ACTUAL_SHA256_HERE"
 
   depends_on "go" => :build
 
